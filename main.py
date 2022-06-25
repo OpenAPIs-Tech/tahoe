16624d7608c8061f79ae3191c08cedfe9beb3fd1
emadrk <emadk3@gmail.com>
2022-06-26 04:24:44 +0530

    add ui

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
+from flask import Flask, render_template, request, url_for, redirect
+from email.mime.text import MIMEText
+import smtplib
+from email.message import EmailMessage
 from flask import Flask, request,Response,render_template
 from flask_cors import CORS, cross_origin
 
 from services.imageurl import genimageurl
+
 
 
 app = Flask(__name__)
@@ -22,6 +27,40 @@ def urlGenerator():
         return Response(json.dumps(response),status=200,mimetype="application/json")
     
     return Response(json.dumps(response),status=400,mimetype="application/json")
+@app.route("/sendemail/", methods=['POST'])
+def sendemail():
+    if request.method == "POST":
+        name = request.form['name']
+        subject = request.form['Subject']
+        email = request.form['_replyto']
+        message = request.form['message']
+
+        your_name = "Emad"
+        your_email = "emadk3@gmail.com"
+        your_password = "Emmurules11"
+
+        # Logging in to our email account
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(your_email, your_password)
+
+        # Sender's and Receiver's email address
+        sender_email = "emadk3@gmail.com"
+        receiver_email = "emadteaches@gmail.com"
+
+        msg = EmailMessage()
+        msg.set_content("First Name : "+str(name)+"\nEmail : "+str(email)+"\nSubject : "+str(subject)+"\nMessage : "+str(message))
+        msg['Subject'] = 'New Response on Personal Website'
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+        # Send the message via our own SMTP server.
+        try:
+            # sending an email
+            server.send_message(msg)
+        except:
+            pass
+    return redirect('/')
 
 if __name__=="__main__":
     app.run()
