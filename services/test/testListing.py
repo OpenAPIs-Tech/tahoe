ce47822a09fde83f612dd6358a0d30e9eab6e12a
Emad Raza Khan <emadrk@Emads-MacBook-Pro.local>
2022-10-07 22:50:10 +0530

    k

--- services/test/testListing.py
+++ services/test/testListing.py
@@ -34,19 +34,22 @@ def getResponseBodyForAllTestsData(dataFromDb):
             if k=='testcode':
                 k='testCode'
 
-            if k=='testname':
+            elif k=='testname':
                 k='testName'
             
-            if k=='testduration':
+            elif k=='testduration':
                 k='testDuration'
 
-            if k=='sectioninfo':
+            elif k=='sectioninfo':
                 k='sectionInfo'
                 v = [d for d in v.split(',')]
             
-            if k == 'sectionmarksinfo':
+            elif k == 'sectionmarksinfo':
                 k='sectionMarksInfo'
                 v = json.loads(v) if v != None else None
+
+            elif k=='test_marks':
+                k='testMarks'
 
             temp[k] = v
         
@@ -60,17 +63,17 @@ def getQueryForAllTests(classId,board,subject,course):
  
     if board and course:
         query = (f'''
-    select a.test_code as testCode,a.class,a.subject,a.course,a.board,b.isdisplayed,b.sectioninfo as sectionInfo,b.sectionmarksinfo as sectionMarksInfo,b.test_name as testName,b.test_duration as testDuration from test_display a join test b on a.test_code = b.testcode where class={classId} and board='{board}' and subject='{subject}' and course = '{course}';
+    select a.test_code as testCode,a.class,a.subject,a.course,a.board,b.isdisplayed,b.sectioninfo as sectionInfo,b.sectionmarksinfo as sectionMarksInfo,b.test_name as testName,b.test_marks,b.test_duration as testDuration from test_display a join test b on a.test_code = b.testcode where class={classId} and board='{board}' and subject='{subject}' and course = '{course}';
     ''')
 
     elif not board:
         query = (f'''
-    select a.test_code as testCode,a.class,a.subject,a.course,a.board,b.isdisplayed,b.sectioninfo as sectionInfo,b.sectionmarksinfo as sectionMarksInfo,b.test_name as testName,b.test_duration as testDuration from test_display a join test b on a.test_code = b.testcode where class={classId} and board is null and subject='{subject}' and course = '{course}';
+    select a.test_code as testCode,a.class,a.subject,a.course,a.board,b.isdisplayed,b.sectioninfo as sectionInfo,b.sectionmarksinfo as sectionMarksInfo,b.test_name as testName,b.test_marks,b.test_duration as testDuration from test_display a join test b on a.test_code = b.testcode where class={classId} and board is null and subject='{subject}' and course = '{course}';
     ''')
 
     elif not course:
         query = (f'''
-    select a.test_code as testCode,a.class,a.subject,a.course,a.board,b.isdisplayed,b.sectioninfo as sectionInfo,b.sectionmarksinfo as sectionMarksInfo,b.test_name as testName,b.test_duration as testDuration from test_display a join test b on a.test_code = b.testcode where class={classId} and board='{board}' and subject='{subject}' and course is null;
+    select a.test_code as testCode,a.class,a.subject,a.course,a.board,b.isdisplayed,b.sectioninfo as sectionInfo,b.sectionmarksinfo as sectionMarksInfo,b.test_name as testName,b.test_marks,b.test_duration as testDuration from test_display a join test b on a.test_code = b.testcode where class={classId} and board='{board}' and subject='{subject}' and course is null;
     ''')
 
     return query
